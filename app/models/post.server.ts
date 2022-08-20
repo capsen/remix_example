b19c635bac8a199a89d8759bb79ddd1715586613
Capsen <capsen_lu@hotmail.com>
2022-08-20 16:38:50 +1000

    add post detail page

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,28 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+// type Post = {
+//   slug: string;
+//   title: string;
+// };
+export type { Post } from "@prisma/client";
+
+export async function getPosts() {
+  return prisma.post.findMany();
 }
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
+
+// export async function getPosts(): Promise<Array<Post>> {
+//   return [
+//     {
+//       slug: "my-first-post",
+//       title: "My First Post",
+//     },
+//     {
+//       slug: "90s-mixtape",
+//       title: "A Mixtape I Made Just For You",
+//     },
+//   ];
+// }
