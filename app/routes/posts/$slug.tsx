c4f3a070fa19a522de3f4a433134dc34d0660afe
Capsen Lu <capsen_lu@hotmail.com>
2022-08-21 22:34:23 +1000

    add maked html

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,17 +1,32 @@
+
+import { marked } from "marked";
 import { json, type LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import type { Post } from "~/models/post.server";
+import { getPost } from "~/models/post.server";
+
+type LoaderData = { post: Post; html: string};
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json({ slug: params.slug });
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown)
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { slug } = useLoaderData();
+  const { post, html } = useLoaderData();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {slug}
+        {post.title}
       </h1>
+      <div dangerouslySetInnerHTML={{__html: html}} />
     </main>
   );
 }
